cece261b2dbb8e987f9f18dbf46f5900e49a10fb
jungkeunmo <abc59684495@gmail.com>
2023-03-26 10:13:08 +0900

    alert

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, TextInput, Button, Alert, StyleSheet } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [text, setText] = useState("");
+  const [password, setPassword] = useState("");
+
+  const clickHandler = () => {
+    Alert.alert(`text : ${text}, password : ${password}`);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.boxContainer}>
+        <TextInput
+          style={{
+            width: 100,
+            marginRight: 50,
+            height: 50,
+            fontSize: 20,
+            backgroundColor: "#ecf0f1",
+            borderRadius: 8,
+          }}
+          onChangeText={(t) => setText(t)}
+          placeholder="A"
+        ></TextInput>
+        <TextInput
+          style={{
+            width: 100,
+            height: 50,
+            fontSize: 20,
+            backgroundColor: "#ecf0f1",
+            borderRadius: 8,
+          }}
+          onChangeText={(t) => setPassword(t)}
+          placeholder="B"
+        ></TextInput>
+      </View>
+      <View style={styles.boxContainer}>
+        <Button title="button" onPress={() => clickHandler()} />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  boxContainer: {
+    width: 400,
+    height: 150,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
